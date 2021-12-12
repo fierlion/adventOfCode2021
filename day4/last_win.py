be46efcce54bb92d8440e8b15d94689d47baabb3
Ray Allan <fierlion@amazon.com>
2021-12-12 00:39:30 +0000

    Add overly complicated day 4 answer

---
+++ day4/last_win.py
@@ -0,0 +1,58 @@
+class BingoNum:
+    number = None 
+    def __init__(self, number):
+        self.number = str(number)
+    def __str__(self):
+        return str(self.number)
+
+class BingoBoard:
+    board = None
+    def __init__(self, board):
+        self.board = board
+    def __str__(self):
+        return str('\n'.join([" ".join((str(num) for num in line)) for line in self.board]))
+    def sum_board(self):
+        # only include horizontal lines
+        positive_ints = [int(j.number) if int(j.number) >= 0 else 0 for sub in self.board[:5] for j in sub]
+        return sum(positive_ints) 
+
+def check_board(board):
+    bingo_string = "-1" * 5
+    for line in board.board:
+        if ("".join(str(num) for num in line) == bingo_string):
+            return True
+    return False
+
+def main():
+    in_file = open('input.txt', 'r')
+    in_lines = in_file.readlines()
+    call_numbers = in_lines[0].strip().split(',')
+    board_size = 5
+    bingo_boards = []
+    nums = {str(num):BingoNum(num) for num in range(100)} 
+
+    for i in range(2, len(in_lines), 6):
+        horiz_board = []
+        vert_board = []
+        for horiz in range(board_size):
+            this_line = in_lines[i+horiz].strip().split()
+            this_line = [nums[num] for num in this_line]
+            horiz_board.append(this_line)
+        for vert in range(board_size):
+            this_line = []
+            for l in range(board_size):
+                this_line.append(horiz_board[l][vert])
+            vert_board.append(this_line)
+        board = horiz_board + vert_board
+        bingo_boards.append(BingoBoard(board))
+    for num in call_numbers:
+    # mark number for all boards
+        nums[num].number = -1
+        for board in bingo_boards:
+            if check_board(board):
+                bingo_boards.remove(board)
+                board_sum = board.sum_board()
+                print(int(num) * board_sum)
+        
+if __name__ == "__main__":
+    main()
